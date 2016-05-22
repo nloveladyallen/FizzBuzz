add4c25b16f848b1a6f2aac20f0b666d603912fa
Noah Lovelady-Allen <noah+github@loveladyallen.com>
2016-05-22 10:31:57 -0400

    Create groovy.groovy

---
+++ groovy.groovy
@@ -0,0 +1,15 @@
+def fizzbuzz = { i ->
+  if(i % 15 == 0) {
+    println "FizzBuzz"
+  } else if(i % 3 == 0) {
+    println "Fizz"
+  } else if(i % 5 == 0) {
+    println "Buzz"
+  } else {
+    println i
+  }
+}
+
+for (i in 1 .. 100) {
+  fizzbuzz(i)
+}
